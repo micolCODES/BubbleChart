421e61af2f6f433c3ddd5f3833ec9fa0adfc0b89
micolCODES <micolromano124@gmail.com>
2023-09-20 11:37:50 +0200

    add simple starting UI with element bubbles navigation links

--- BubbleChart/ContentView.swift
+++ BubbleChart/ContentView.swift
@@ -9,13 +9,76 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                HStack{}
+                VStack{
+                    HStack{
+                        Spacer()
+                        NavigationLink {
+                            
+                        } label: {
+                            Text("Fire")
+                                .frame(width: 130, height: 130)
+                                .font(.title.bold())
+                                .foregroundColor(Color.white)
+                                .background(Color.red.gradient)
+                                .clipShape(Circle())
+                        }
+                        Spacer()
+                    }
+                    HStack{
+                        NavigationLink {
+                            
+                        } label: {
+                            Text("Wood")
+                                .frame(width: 130, height: 130)
+                                .font(.title.bold())
+                                .foregroundColor(Color.black)
+                                .background(Color.green.gradient)
+                                .clipShape(Circle())
+                        }
+                        Spacer()
+                        NavigationLink {
+                            
+                        } label: {
+                            Text("Earth")
+                                .frame(width: 130, height: 130)
+                                .font(.title.bold())
+                                .foregroundColor(Color.black)
+                                .background(Color.orange.gradient)
+                                .clipShape(Circle())
+                        }
+                    }
+                    HStack{
+                        Spacer()
+                        NavigationLink {
+                            
+                        } label: {
+                            Text("Water")
+                                .frame(width: 130, height: 130)
+                                .font(.title.bold())
+                                .foregroundColor(Color.white)
+                                .background(Color.black.gradient)
+                                .clipShape(Circle())
+                        }
+                        NavigationLink {
+                            
+                        } label: {
+                            Text("Metal")
+                                .frame(width: 130, height: 130)
+                                .font(.title.bold())
+                                .foregroundColor(Color.black)
+                                .background(Color(UIColor.lightGray).gradient)
+                                .clipShape(Circle())
+                        }
+                        Spacer()
+                    }
+                }
+                HStack{}
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
