b1c5ffdcf46b24a38155021bbf56a3246235b023
micolCODES <micolromano124@gmail.com>
2023-09-20 10:53:27 +0200

    Initial Commit

---
+++ BubbleChart/BubbleChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  BubbleChartApp.swift
+//  BubbleChart
+//
+//  Created by Micol on 20.09.23.
+//
+
+import SwiftUI
+
+@main
+struct BubbleChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BubbleChartUITests/BubbleChartUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BubbleChartUITestsLaunchTests.swift
+//  BubbleChartUITests
+//
+//  Created by Micol on 20.09.23.
+//
+
+import XCTest
+
+final class BubbleChartUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
