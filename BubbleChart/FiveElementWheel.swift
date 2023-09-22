9dcc31284ca159a2f503fea60bb913ed6f0961ad
micolCODES <micolromano124@gmail.com>
2023-09-22 09:33:19 +0200

    made separate file for FiveElementWheel
    add Yin bool to set if placing points should start at wood or metal

---
+++ BubbleChart/FiveElementWheel.swift
@@ -0,0 +1,88 @@
+//
+//  FiveElementWheel.swift
+//  BubbleChart
+//
+//  Created by Micol on 22.09.23.
+//
+
+import Foundation
+import SwiftUI
+
+struct FiveElementWheel: View {
+    var yin: Bool
+    
+    let meridians: [String: Meridian] = Bundle.main.decode("meridiansNacupoints.json")
+    
+    var body: some View {
+        VStack{
+            HStack{
+                Spacer()
+                NavigationLink {
+                    ElementView(element: "fire")
+                } label: {
+                    Text("Fire")
+                        .frame(width: 120, height: 120)
+                        .font(.title.bold())
+                        .foregroundColor(Color.white)
+                        .background(RadialGradient(gradient: Gradient(colors: [ElementColor.fire.opacity(0.6), ElementColor.fire, .black]), center: .center, startRadius: 0, endRadius: 120))
+                        .clipShape(Circle())
+                }
+                Spacer()
+            }
+            HStack{
+                NavigationLink {
+                    ElementView(element: "wood")
+                } label: {
+                    Text("Wood")
+                        .frame(width: 120, height: 120)
+                        .font(.title.bold())
+                        .foregroundColor(Color.black)
+                        .background(RadialGradient(gradient: Gradient(colors: [ElementColor.wood.opacity(0.4), ElementColor.wood, .black]), center: .center, startRadius: 0, endRadius: 120))
+                        .clipShape(Circle())
+                }
+                HStack {
+                    Text("\(meridians.count == 12 ? "JSON ✅" : "Cannot read JSON")")
+                        .frame(width: 120, height: 120)
+                        .font(.headline.bold())
+                        .foregroundColor(Color.black)
+                        .background(Color.white.gradient)
+                        .clipShape(Circle())
+                }
+                NavigationLink {
+                    ElementView(element: "earth")
+                } label: {
+                    Text("Earth")
+                        .frame(width: 120, height: 120)
+                        .font(.title.bold())
+                        .foregroundColor(Color.black)
+                        .background(RadialGradient(gradient: Gradient(colors: [ElementColor.earth.opacity(0.4), ElementColor.earth, .black]), center: .center, startRadius: 0, endRadius: 120))
+                        .clipShape(Circle())
+                }
+            }
+            HStack{
+                Spacer()
+                NavigationLink {
+                    ElementView(element: "water")
+                } label: {
+                    Text("Water")
+                        .frame(width: 120, height: 120)
+                        .font(.title.bold())
+                        .foregroundColor(Color.white)
+                        .background(RadialGradient(gradient: Gradient(colors: [ElementColor.water.opacity(0.5), ElementColor.water, .black]), center: .center, startRadius: 0, endRadius: 120))
+                        .clipShape(Circle())
+                }
+                NavigationLink {
+                    ElementView(element: "metal")
+                } label: {
+                    Text("Metal")
+                        .frame(width: 120, height: 120)
+                        .font(.title.bold())
+                        .foregroundColor(Color.black)
+                        .background(RadialGradient(gradient: Gradient(colors: [ ElementColor.metal.opacity(0.1),ElementColor.metal, .black]), center: .center, startRadius: 0, endRadius: 120))
+                        .clipShape(Circle())
+                }
+                Spacer()
+            }
+        }
+    }
+}
