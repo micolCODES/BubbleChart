52a651a5ea0b99ef4d22403a5eb13c3a0d432174
micolCODES <micolromano124@gmail.com>
2023-09-23 18:42:59 +0200

    remove NavigationView from ElementView to fix UI, modify ElementView so Fire looks like other elements

--- BubbleChart/ElementView.swift
+++ BubbleChart/ElementView.swift
@@ -13,11 +13,18 @@ struct ElementView: View {
     @State private var circleSize = CGFloat(50)
     
     var element: String
+    let meridians: [String: Meridian] = Bundle.main.decode("meridiansNacupoints.json")
     
     
     var body: some View {
-        NavigationView {
+        VStack {
+            VStack{
+                Text(element.capitalized)
+                    .font(.title.bold())
+            }
+            .padding(.top)
             ZStack {
+                //for normal elements with 2 meridians
                 if element != "fire" {
                     Circle()
                     //color will depend on element chosen
@@ -35,12 +42,14 @@ struct ElementView: View {
                         .frame(maxWidth: .infinity)
                         Spacer()
                     }
-                } else {
+                } else { //for the fire element that has 4 meridians
                     Rectangle()
                     //color will depend on element chosen
                         .fill(RadialGradient(gradient: Gradient(colors: [elementColorPicker(element).opacity(0.1), elementColorPicker(element), .black.opacity(0.3)]), center: .center, startRadius: 0, endRadius: 400))
-                        .cornerRadius(40)
+                        .frame(height: 480)
+                        .cornerRadius(150)
                     VStack {
+                        Spacer()
                         HStack {
                             //LEADING point system, SI
                             FiveElementWheel(wood: "XXX", fire: "XXX", earth: "XXX", metal: "XXX", water: "XXX", master: "XXX", yin: false, circleSize: circleSize, typeOfView: "ElementView")
@@ -58,31 +67,30 @@ struct ElementView: View {
                                 .font(.title2.bold())
                         }
                         .frame(maxWidth: .infinity)
+                        Spacer()
                     }
                 }
             }
             .padding()
-            //needs to be VARIABLE with element chosen
-            .navigationTitle(element.capitalized)
         }
     }
-    
-    func elementColorPicker (_ element: String) -> Color{
-        switch element {
-        case "wood": return ElementColor.wood
-        case "fire": return ElementColor.fire
-        case "earth": return ElementColor.earth
-        case "metal": return ElementColor.metal
-        case "water": return ElementColor.water
-        default: return Color.white
-        }
-    }
-    
-    func elementAcupointsPicker(_ element: String) {
         
-    }
+        func elementColorPicker (_ element: String) -> Color{
+            switch element {
+            case "wood": return ElementColor.wood
+            case "fire": return ElementColor.fire
+            case "earth": return ElementColor.earth
+            case "metal": return ElementColor.metal
+            case "water": return ElementColor.water
+            default: return Color.white
+            }
+        }
+        
+        func elementAcupointsPicker(_ element: String) {
+            
+        }
 }
 
 #Preview {
-    ElementView(element: "water")
+    ElementView(element: "fire")
 }
