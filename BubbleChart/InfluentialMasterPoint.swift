b91541b012dc8e310d8fe40297099f80f3c455d4
micolCODES <micolromano124@gmail.com>
2023-11-23 17:27:54 +0100

    add new struct to decode Influential Points JSON

---
+++ BubbleChart/InfluentialMasterPoint.swift
@@ -0,0 +1,14 @@
+//
+//  InfluentialMasterPoint.swift
+//  BubbleChart
+//
+//  Created by Micol on 19.11.23.
+//
+
+import Foundation
+
+struct InfluentialAndMasterPoint: Codable, Identifiable {
+    let id: String
+    let target: String
+    let description: String
+}
