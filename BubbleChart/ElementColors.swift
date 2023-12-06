942eee0f313a4b7f8b350ccd97d962fba98268cb
micolCODES <micolromano124@gmail.com>
2023-12-06 20:14:25 +0100

    create ElementColor separate file (migrated from ContentView)

---
+++ BubbleChart/ElementColors.swift
@@ -0,0 +1,27 @@
+//
+//  ElementColors.swift
+//  BubbleChart
+//
+//  Created by Micol on 05.12.23.
+//
+
+import Foundation
+import SwiftUI
+
+struct ElementColor{
+    static let wood = Color("wood")
+    static let lightwood = Color("lightwood")
+    static let fire = Color("fire")
+    static let lightfire = Color("lightfire")
+    static let earth = Color("earth")
+    static let lightearth = Color("lightearth")
+    static let metal = Color("metal")
+    static let lightmetal = Color("lightmetal")
+    static let water = Color("water")
+    static let lightwater = Color("lightwater")
+    static let influential = Color("influential")
+    static let lightinfluential = Color("lightinfluential")
+    static let master = Color("master")
+    static let lightmaster = Color("lightmaster")
+    
+}
