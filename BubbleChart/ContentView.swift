5e80aa2d48edc19f1abd8d5256d94d0c2a03ce92
micolCODES <micolromano124@gmail.com>
2023-12-11 19:32:36 +0100

    add background color for neumorphic theme, update toggle for species selection

--- BubbleChart/ContentView.swift
+++ BubbleChart/ContentView.swift
@@ -9,55 +9,62 @@ import SwiftUI
 
 
 struct ContentView: View {
-    @State private var isHorse = true
     
     @StateObject var specie = SetSpecie()
     
     var body: some View {
         NavigationStack {
-            VStack {
-                Spacer()
-                Spacer()
-                ZStack {
-                    Circle()
-                        .fill(.white)
-                        .stroke(.gray.opacity(0.15), style: StrokeStyle(lineWidth: 10))
-                        .frame(width: 300, height: 300)
-//                        StarShape()
-//                            .stroke(.black, style: StrokeStyle(lineWidth: 1, lineJoin: .miter))
-//                            .rotationEffect(Angle(degrees: 17))
-//                            .frame(width: 200, height: 200)
-                    FiveElementWheelElements()
-                        .font(.title.bold())
-                }
-                Spacer()
-                Spacer()
-                //Add meridian charts
+            ZStack {
+                Color(ElementColor.offWhite)
+                    .ignoresSafeArea()
                 VStack {
-                    Toggle(isOn: $isHorse, label: {
-                        HStack{
-                            if isHorse {
-                                Image("horse")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                            } else {
-                                Image("smallanimals2")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                            }
-                        }
-                        .frame(maxHeight: 80)
-                    })
-                    .onChange(of: isHorse){
-                        specie.specie = (isHorse ? "horse" : "smallanimals")
+                    Spacer()
+                    Spacer()
+                    ZStack {
+                        Circle()
+//                            .stroke(.gray.opacity(0.15), style: StrokeStyle(lineWidth: 10))
+                            .stroke(ElementColor.offWhite, style: StrokeStyle(lineWidth: 6))
+                            .frame(width: 300, height: 300)
+                            .shadow(color: Color.black.opacity(0.3), radius: 8, x: 5, y: 5)
+                            .shadow(color: Color.white, radius: 8, x: -5, y: -5)
+                        //                        StarShape()
+                        //                        .stroke(.gray.opacity(0.15), style: StrokeStyle(lineWidth: 10, lineJoin: .miter))
+                        //                            .rotationEffect(Angle(degrees: 17))
+                        //                            .frame(width: 330, height: 320)
+                        FiveElementWheelElements()
+                            .font(.title.bold())
                     }
+                    Spacer()
+                    Spacer()
+                    //Add meridian charts
+                    VStack {
+                        Toggle(isOn: $specie.specie, label: {
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 10)
+                                    .shadow(color: Color.black.opacity(0.2), radius: 8, x: 8, y: 8)
+                                    .shadow(color: Color.white, radius: 8, x: -5, y: -5)
+                                HStack{
+                                    if specie.specie {
+                                        Image("horseOffWhite")
+                                            .resizable()
+                                            .aspectRatio(contentMode: .fit)
+                                    } else {
+                                        Image("smallanimalsOffWhite")
+                                            .resizable()
+                                            .aspectRatio(contentMode: .fit)
+                                    }
+                                }
+                            }
+                            .frame(maxWidth: 120,maxHeight: 120)
+                        })
                         .toggleStyle(.button)
                         .font(.system(size: 100))
-                        .tint(.white)
+                        .tint(ElementColor.offWhite)
+                    }
                 }
+                .padding()
+                //.navigationTitle("5 Elements")
             }
-            .padding()
-            //.navigationTitle("5 Elements")
         }
         .environmentObject(specie)
     }
