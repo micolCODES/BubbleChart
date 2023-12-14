b8b8bf58c161fda1ee7956ec2067179f774095a9
micolCODES <micolromano124@gmail.com>
2023-12-14 17:40:10 +0100

    add long description, add test image, update UI to neumorphic

--- BubbleChart/ExtraAcupointView.swift
+++ BubbleChart/ExtraAcupointView.swift
@@ -15,60 +15,60 @@ struct ExtraAcupointView: View {
     
     var body: some View {  
         ZStack {
-        Color(ElementColor.offWhite)
-            .ignoresSafeArea()
-        GeometryReader { geometry in
-            ScrollView {
-                let acupointInfo = point
-                let specieOfImages = specie.specie ? "horse" : "smallanimals"
-                let pointName = "\(specieOfImages)-\(acupointInfo.id)"
-
-//                let level = setPoint(yin: yin, pointElement: pointElement)
-              
-                    VStack {
-                        ScrollView(.horizontal){
-                            HStack{
-                                Image("placeholder")
-                                    .resizable()
-                                    .scaledToFit()
-                                Image("placeholder")
-                                    .resizable()
-                                    .scaledToFit()
+            let acupointInfo = point
+            let specieOfImages = specie.specie ? "horse" : "smallanimals"
+            let pointName = "\(specieOfImages)-\(acupointInfo.id)"
+            
+            Color(ElementColor.offWhite)
+                .ignoresSafeArea()
+            
+            
+            VStack{
+                GeometryReader { geometry in
+                    ScrollView {
+                        VStack {
+                            ScrollView(.horizontal){
+                                HStack{
+                                    Image("test")
+                                        .resizable()
+                                        .scaledToFit()
+                                }
                             }
                         }
-                        VStack(alignment: .leading){
-                            Text("\(acupointInfo.id)")
-                            Text("Target: \(acupointInfo.target)")
-                            Text("Description: \(acupointInfo.description)")
-                            Text("Image name: \(pointName)")
-                        }
-                        .padding()
-                        VStack {
-                            Toggle(isOn: $specie.specie, label: {
-                                ZStack {
-                                    RoundedRectangle(cornerRadius: 10)
-                                        .shadow(color: Color.black.opacity(0.2), radius: 8, x: 8, y: 8)
-                                        .shadow(color: Color.white, radius: 8, x: -5, y: -5)
-                                    HStack{
-                                        if specie.specie {
-                                            Image("horseOffWhite")
-                                                .resizable()
-                                                .aspectRatio(contentMode: .fit)
-                                        } else {
-                                            Image("smallanimalsOffWhite")
-                                                .resizable()
-                                                .aspectRatio(contentMode: .fit)
-                                        }
-                                    }
-                                }
-                                .frame(maxWidth: 120, maxHeight: 120)
-                            })
-                            .toggleStyle(.button)
-                            .font(.system(size: 100))
-                            .tint(.white)
-                        }
+                    VStack(alignment: .leading){
+                        Text("\(acupointInfo.id)")
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                            .font(.headline)
+                        Text("Target: \(acupointInfo.target)")
+                        Text("Description: \(acupointInfo.description)")
+                        Text("Image name: \(pointName)")
+                        Text("Long Text: Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.")
+                    }
+                    .padding(.horizontal)
                     }
                 }
+                Spacer()
+                Spacer()
+                Toggle(isOn: $specie.specie, label: {
+                    HStack{
+                        if specie.specie {
+                            Image("horseOffWhite")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                        } else {
+                            Image("smallanimalsOffWhite")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                        }
+                    }
+                    .frame(maxWidth: 80,maxHeight: 60)
+                    .clipShape(RoundedRectangle(cornerSize: CGSize(width: 5, height: 5)))
+                    .shadow(color: Color.black.opacity(0.2), radius: 5, x: 5, y: 5)
+                    .shadow(color: Color.white, radius: 8, x: -5, y: -5)
+                })
+                .toggleStyle(.button)
+                .font(.system(size: 100))
+                .tint(ElementColor.offWhite)
             }
         }
     }
